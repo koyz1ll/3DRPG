8045b100c2c042cee57f9d3e1519c4c12c685694
zhiwen.li <674356245@qq.com>
2022-12-29 10:16:33 +0800

    30:Create Portal 创建传送门

---
+++ Assets/Scripts/Transition/TransitionDestination.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class TransitionDestination:MonoBehaviour
+{
+    public enum DestinationTag
+    {
+        Enter,A,B,C
+    }
+
+    public DestinationTag destinationTag;
+}
---
+++ Assets/Scripts/Transition/TransitionPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class TransitionPoint:MonoBehaviour
+{
+    public enum TransitionType
+    {
+        SameScene,DifferentScene
+    }
+    [Header("Transition Info")] 
+    public string sceneName;
+
+    public TransitionType transitionType;
+    public TransitionDestination.DestinationTag destinationTag;
+
+    private bool canTrans;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.layer.Equals(LayerUtils.Player))
+        {
+            canTrans = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer.Equals(LayerUtils.Player))
+        {
+            canTrans = false;
+        }
+    }
+}
